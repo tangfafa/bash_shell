7d41758fd2933954ad927c1fb733dc90d5084a2e
tangfafa <18782468140@163.com>
2019-01-06 02:05:26 +0800

    添加demo.sh

---
+++ demo.sh
@@ -0,0 +1,14 @@
+pwd # 确认一下当前路径是不是 ~/local 或者 /c/Users/你的名字/local
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  touch index.html css/style.css js/main.js
+  echo '<!DOCTYPE><title>Hello</title><h1>Hi</h1>' >> index.html
+  echo 'h1{color: red;}' >> css/style.css
+  echo 'var string = "Hello World";alert(string)' >> js/main.js
+  exit
+fi
